1b296608c023cbec223a4f40897332b5cdc5f46e
Eridanus-ui <njigupaul22@gmail.com>
2025-02-26 19:48:06 +0300

    🔧 update CORS origin to pneuma-estate.vercel.app

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 
 // _Enable CORS middleware
 app.use(cors({
-  origin: ['https://stlc.vercel.app', 'http://localhost:5173'],
+  origin: ['https://pneuma-estate.vercel.app', 'http://localhost:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
 }));
 
